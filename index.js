ec4fa3f4b039dde8b2618ed42794d0d5ab2e5fc7
Vishnu <vishnuraj.k.v0@gmail.com>
2021-04-10 08:06:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('Subdomain Takeover By vishnu!\n');
+  res.end('Subdomain Takeover By vishnu!\n bugcrowd.com/vishnuraj ');
 });
 
 server.listen(port, () => {
